e9006bc2a514111652269f8ba6c8cd4158fba018
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-04-12 08:01:26 +0900

    bundle gem testjoy

---
+++ lib/testjoy/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Testjoy
+  VERSION = "0.1.0"
+end
---
+++ spec/testjoy_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Testjoy do
+  it "has a version number" do
+    expect(Testjoy::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
